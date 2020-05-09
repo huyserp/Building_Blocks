56ac4ce4eac41d77cc6f5d8edabf0c46d82365f8
Peter Huyser <huyserp@gmail.com>
2020-05-09 12:35:55 +0200

    retroactively added problem solving plan and algorithm

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -1,26 +1,53 @@
-# def caesar_cipher(phrase, shift)
-#     split_phrase = phrase.split('')
+=begin
 
-#     new_split_phrase = split_phrase.each do |letter|
-#         if /\W/ =~ letter
-#             letter
-#         else
-#             shift.times do
-#                 letter.next!
-#             end
-#             if letter.length > 1
-#                 letter.delete!(letter[0])
-#             end
-#             letter
-#         end
-#     end
+### THE PROBLEM ###
+Write a program that takes a message and an integer value as inputs.
+manipulate the message by sliding each letter the number of positions in the alphabet indicated 
+by the value of the integer input.  positive, slide right though the alphabet, negative slide left.
+output a new message that is a transformation of the input message with the shift applied.
 
-#     new_phrase = new_split_phrase.join()
-#     return new_phrase
-# end
+### PLAN ###
+This problem does not have an interface, it will be called and run from the command line.
 
+This program will take 2 inputs, a message and an integer value that will identify how many values each character in the 
+message will change. The 2 inputs will be parameters to the method, creating two local variables and the user will pass them 
+to the method as arguments.
+
+The desired output is a message that is the same length as the one given, but each character will be a new character that is 
+the number of positions within the alphabet away from the original character indicated by the integer value given as the second input.
+
+
+### THE ALGORITHM ###
+define a method that takes two inputs as local variables: phrase and shift.
+
+phrase is a string message of any length; shift is a positive or negative integer.
+
+break the phrase apart into individual characters and store this new array in a varible called 'split_phrase'.
+
+loop over all of the items in 'split_phrase' and apply a change to each one.  each character will be called 'letter':
+  if 'letter' is not a letter in the alphabet, leave it as it is, it is probably a space or punctuation
+
+  if 'letter' is lowercase, convert it to its unicode integer and add to it the value stored in in the 'shift' variable.
+  store this new value in a variable called 'slide'.
+
+  if the value of slide is greater than the unicode value of 'z' then take the difference of the two numeric values and 
+  add that to the beginning of the alphabet, so as to "go around the 'U'".
+
+  convert the integer value stored in 'slide' back to a letter and store it back in the original 'letter' variable.
+
+  If 'letter' is a capital letter, we want to make sure we maintain the same case. repeat the same steps as when 'letter' is lowercase, but 
+  using the unicode numbers that correspond to uppercase letters.
+
+after the loop has completed we should have a new array of characters that represent a change indicated by the value input to 'shift'
+
+push the mutated array, saved in 'split_pharse', back together into a string and return it as the output.
+
+Numeric values corresponding to the Characters in the alphabet:
      # A-Z        65 - 90
      # a-z        97 - 122
+
+=end
+
 
 def caesar_cipher(phrase, shift)
    split_phrase = phrase.split("")
@@ -62,3 +89,25 @@ puts puts
 puts "Caesar_Cipher: Please, kind sir, would you point me to the nearest exit?; slide -3"
 puts caesar_cipher("Please, kind sir, would you point me to the nearest exit?", -3)
 puts puts
+
+#ALTERNATE PROGRAM - DOESNT ALLOW BACK AND FORTH, OR ARUOND THE "U"
+# def caesar_cipher(phrase, shift)
+#     split_phrase = phrase.split('')
+
+#     new_split_phrase = split_phrase.each do |letter|
+#         if /\W/ =~ letter
+#             letter
+#         else
+#             shift.times do
+#                 letter.next!
+#             end
+#             if letter.length > 1
+#                 letter.delete!(letter[0])
+#             end
+#             letter
+#         end
+#     end
+
+#     new_phrase = new_split_phrase.join()
+#     return new_phrase
+# end
--- Substrings.rb
+++ Substrings.rb
@@ -1,3 +1,48 @@
+
+=begin
+### THE PROBLEM ###
+
+Write a program that takes a message, and checks a given 'dictionary' which contains a collection of words, or substrings
+to see how many different words in that dictionary can be found within the given message. 
+
+
+### THE PLAN ###
+
+This program does not have a user interface, it will be run from the command line.
+
+The program will take two inputs, the first being a message of any length, the second being a collection of words or phrases aka 'dictionary'.  Both inputs will be entered as arguments to the defined method.
+
+The desired output is a hash that lists all the words or phrases from the 'dictionary' collection that can be found within the message.
+Each found item from the dictionary will be stored as a 'key' in the hash, the number of different times each item can be found in the message
+will be stored as that key's value.
+
+### THE ALGORITHM ###
+
+create an array that holds a collection of words and/or substrings that you want to check for - store this array in a varible called 'dictionary'.
+
+define a method that takes two parameters - one is a message that you want to inspect, the other is the dictionary you previously created. 
+The message that you want to check will be stored in a local variable called 'check'.
+
+within the method make sure that your given message and the dictionary all consist of the the same case - so that they can be compared.  
+All upcase or all downcase. set the message in 'check' to downcase - store this in a new variable called 'phrase'
+
+create the empty hash that will hold the values to be output.  call this 'substrs'.
+
+loop through 'dictionary' and look at each item.  within this loop each item will be nameed 'word'.
+
+if 'phrase' includes the 'word' in the current itoration, create a key in 'substrs' that is that particular 'word'.
+set the value of this particular 'word' key to be the number of times that 'word' can be found in our 'phrase'.
+    to do this, look through all of 'phrase' for the specifc 'word' and count how many times 'word' appears this result will be your value.
+
+after you've itorated competely though 'dictionary', the 'substrs' hash will be populated with key: value pairs of items from 'dictionary' and how many times
+they appear in 'phrase'
+
+return the hash substrs as the output of your program.
+    
+=end
+
+
+
 dictionary = [
     "below","down","go",
     "going","horn","how",
